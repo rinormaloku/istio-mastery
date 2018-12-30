9bb9cfbed20246ff03ed70b7b867a2548af8b980
rinormaloku <rinormaloku37@gmail.com>
2018-12-30 15:06:25 +0100

    istio resources

--- sa-logic/sa/sentiment_analysis.py
+++ sa-logic/sa/sentiment_analysis.py
@@ -1,14 +1,21 @@
+import logging
+import sys
+import random
+import time 
 from os import getenv
 from textblob import TextBlob
 from flask import Flask, request, jsonify
 from flask_api import status
-from jaeger_client import Config
 from flask_opentracing import FlaskTracer
+from jaeger_client import Config
 
 JAEGER_HOST = getenv('JAEGER_HOST', 'localhost')
 JAEGER_SERVICE_NAME = getenv('JAEGER_SERVICE_NAME', 'sa-logic')
 
 app = Flask(__name__)
+
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.DEBUG)
 
 config = Config(config={'sampler': {'type': 'const', 'param': 1},
                         'logging': True,
@@ -24,13 +31,21 @@ tracer = FlaskTracer(jaeger_tracer)
 @app.route("/analyse/sentiment", methods=['POST'])
 @tracer.trace()
 def analyse_sentiment():
-    sentence = request.get_json()['sentence']
-    polarity = TextBlob(sentence).sentences[0].polarity
-    return jsonify(
-        sentence=sentence,
-        polarity=polarity
-    )
+    random_number = random.randint(1,101)
+    if random_number < 34: 
+        app.logger.debug('This failed badly')
+        content = 'this was never meant to happen, please hide'
+        return content, status.HTTP_500_INTERNAL_SERVER_ERROR
+    elif random_number < 66:
+        time.sleep(30)
+    else:
+        sentence = request.get_json()['sentence']
+        polarity = TextBlob(sentence).sentences[0].polarity
+        return jsonify(
+            sentence=sentence,
+            polarity=polarity
+        )
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
